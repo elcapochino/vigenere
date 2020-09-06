8435c0df18f95c514f0d80aa3014c1e9b289334a
Peter <lecapochino@gmail.com>
2020-09-07 00:12:33 +0200

    united encrypt and decrypt functions

--- vigenere.py
+++ vigenere.py
@@ -2,10 +2,18 @@ import string
 import time
 
 
-def encrypt(plaintext, key, charlist=None, space=""):
-    
+charlist = string.ascii_uppercase
+
+class CryptModes:
+    encrypt = 1
+    decrypt = 2
+
+
+def crypt(text_source, key, mode, charlist=None, space=None):
+
     # input validation
-    if not plaintext or type(plaintext) is not str:
+    # note: if not [string] checks for empty string
+    if not text_source or type(text_source) is not str:
         raise ValueError("Invalid plaintext")
     if not key or type(key) is not str:
         raise ValueError("Invalid key")
@@ -20,127 +28,87 @@ def encrypt(plaintext, key, charlist=None, space=""):
     # if no valid charlist defined, choose default charlist (i.e. A-Z)
     if not charlist:
         charlist = string.ascii_uppercase
-        # print("default charlist")
     
-    # ensure that all chars are uppercase
-    plaintext = plaintext.upper()
-    key = key.upper()
-    charlist = charlist.upper()
-    space = space.upper()
-        
-    print("charlist: " + charlist + "\n")
-    
-    # replace spaces with space char or remove them
-    if space:
-        plaintext = plaintext.replace(" ", space)
-        
+    # replace spaces with space char if encrypting
+    if mode == CryptModes.encrypt:
+        text_source = text_source.replace(" ", space if space else '')
+
     # ensure that all chars are in charlist
-    invalid_plain_chars = set()
-    for i in plaintext:
-        if i not in charlist and i not in invalid_plain_chars:
-            invalid_plain_chars.add(i)
-            print("WARN: plaintext char '%s' is not in the charlist and will be removed" % i)
-    for i in key:
-        if i not in charlist:
-            raise ValueError("Key char '%s' not in charlist" % i)
+    invalid_chars = set()
+    for char_ in text_source:
+        if char_ not in charlist and char_ not in invalid_chars:
+            invalid_chars.add(char_)
+            print(f"WARNING: text char '{char_}' is not in the charlist and will be removed")
+    for char_ in key:
+        if char_ not in charlist:
+            raise ValueError(f"Key char '{char_}' not in charlist")
     if space and space not in charlist:
         raise ValueError('Space char not in charlist')
     
-    # plaintext may only contain chars available in charlist
-    plaintext = ''.join(filter(charlist.__contains__, plaintext))
-    
-    # encrypt
-    ciphertext = ""
-    for i, _char in enumerate(plaintext):
-        char_shift = key[i % len(key)]
-        index_shift = charlist.index(char_shift)
-        
-        index_plain = charlist.index(_char)
-        index_cipher = (index_plain + index_shift) % len(charlist)
-        ciphertext += charlist[index_cipher]
-    
-    return ciphertext
-
-
-def decrypt(ciphertext, key, charlist=None, space=""):
-
-    # input validation
-    if not ciphertext or type(ciphertext) is not str:
-        raise ValueError("Invalid plaintext")
-    if not key or type(key) is not str:
-        raise ValueError("Invalid key")
-    if charlist and type(charlist) is not str:
-        raise ValueError("Invalid charlist")
-    if space:
-        if type(space) is not str:
-            raise ValueError("Invalid space char")
-        if len(space) > 1:
-            raise ValueError('Too many space chars')
-    
-    # if no valid charlist defined, choose default charlist (i.e. A-Z)
-    if not charlist:
-        charlist = string.ascii_uppercase
-        # print("default charlist")
-    
-    # ensure that all chars are uppercase
-    ciphertext = ciphertext.upper()
-    key = key.upper()
-    charlist = charlist.upper()
-    space = space.upper()
-        
-    print("charlist: " + charlist + "\n")
-    
-    # decrypt
-    plaintext = ""
-    for i, _char in enumerate(ciphertext):
-        char_shift = key[i % len(key)]
-        index_shift = charlist.index(char_shift)
-        
-        index_cipher = charlist.index(_char)
-        index_plain = (index_cipher - index_shift) % len(charlist)
-        plaintext += charlist[index_plain]
-    
-    # replace space char with actual space
-    if space:
-        plaintext = plaintext.replace(space, " ")
-        
-    return plaintext
+    # text may only contain chars available in charlist
+    text_source = ''.join(filter(charlist.__contains__, text_source))
     
 
-# example input: "a-z0-9öü-"
-def get_charlist(_input):
-    _input = _input.upper()
+    # actual encryption/decryption
+    text_result = ""
+    for i, char_source in enumerate(text_source):
+        char_key = key[i % len(key)]
+        index_key = charlist.index(char_key)
+        
+        index_source = charlist.index(char_source)
+        
+        if mode == CryptModes.encrypt:
+            index_result = (index_source + index_key) % len(charlist)
+        elif mode == CryptModes.decrypt:
+            index_result = (index_source - index_key) % len(charlist)
+        else:
+            raise ValueError("Invalid crypt mode")
+            
+        text_result += charlist[index_result]
+    
+    
+    # replace space chars with spaces if decrypting
+    if space and mode == CryptModes.decrypt:
+        text_result = text_result.replace(space, ' ')
+        
+    return text_result
+        
+
+def get_charlist(input_):
+    REPLACE = {
+        "A-Z": string.ascii_uppercase,
+        "a-z": string.ascii_lowercase,
+        "0-9": string.digits
+    }
+
+    # input_ = input_.upper()
     
     charlist = ""
-    for i in range(len(_input)):
-        if _input[i:i+3] == "A-Z":
-            _input = _input.replace("A-Z", "***")
-            charlist += ''.join([a for a in string.ascii_uppercase if a not in charlist])
+    i = 0
+    while i < len(input_):
+        # scan if there are any REPLACE keys starting from index i
+        for j in range(i+1, len(input_)+1):
+            key = input_[i:j]
+            if key in REPLACE:
+                charlist += ''.join([a for a in REPLACE[key] if a not in charlist])
+                i += len(key)
+                break
+        # if no REPLACE keys found, add single char
+        else:
+            charlist += input_[i] if input_[i] not in charlist else ''
+            i += 1
             
-        elif _input[i:i+3] == "0-9":
-            _input = _input.replace("0-9", "***")
-            charlist += ''.join([a for a in string.digits if a not in charlist])
-            
-        elif _input[i] not in "* " and _input[i] not in charlist:
-            charlist += _input[i]
+    return charlist
     
-    return charlist #  if len(charlist) > 0 else None
-    
-    
+
 if __name__ == "__main__":
-    msg = input("Plaintext: ")
+    msg = input("Text: ")
     key = input("Key: ")
     charlist = get_charlist(input("Charlist (optional): "))
     space = input("Space char (optional): ")
-    ciphermsg = encrypt(msg, key, charlist=charlist, space=space)
-    print("Ciphertext:", ciphermsg)
-    print("press enter to decrypt")
-    input()
+    mode_input = input("(1) encrypt   (2) decrypt     : ")
+    mode = CryptModes.encrypt if mode_input == "1" else CryptModes.decrypt
     
-    msg = input("Ciphertext: ")
-    key = input("Key: ")
-    charlist = get_charlist(input("Charlist (optional): "))
-    space = input("Space char (optional): ")
-    print(decrypt(msg, key, charlist=charlist, space=space))
-    
+    print(crypt(msg, key, mode, charlist=charlist, space=space))
+
     
